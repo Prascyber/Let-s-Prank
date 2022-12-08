2de968d3faecbac753e85890fad00c5dff7b2efe
Prashant Gupta <76205868+Prascyber@users.noreply.github.com>
2022-12-08 23:03:06 +0000

    Update Prank.js

--- Prank.js
+++ Prank.js
@@ -1,5 +1,3 @@
-let dice = Math.trunc(Math.random() * 6) + 1;
-
-while(dice !==6){
-console.log(`I want to fuck you  ${dice} times`);
+for(let i =0; i<=100000000000000; i++){
+    alert(`Hi Buddy Fuck You ${i}`)
 }
